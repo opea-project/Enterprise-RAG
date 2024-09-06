2d6ac5ded9342d81c40f07cc03c63edab37f9152
Mateusz Blaszkowski <mateusz.blaszkowski@intel.com>
2024-09-06 13:51:45 +0200

    Add live logging to microservices tests (#98)
    Signed-off-by: Mateusz Blaszkowski <mateusz.blaszkowski@intel.com>

--- src/tests/microservices/conftest.py
+++ src/tests/microservices/conftest.py
@@ -4,16 +4,31 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import pytest
+import subprocess # nosec B404
 from pathlib import Path
-from subprocess import run # nosec B404
 
 
 @pytest.fixture
 def assert_bash_test_succeeds():
     def _run_bash_test(test_file):
         bash_test = Path.cwd().joinpath(test_file)
-        result = run(['bash', bash_test], capture_output=True, text=True)
-        print(f"Stdout from bash test: {result.stdout}")
-        assert result.returncode == 0, f"{test_file} failed. Captured stderr: {result.stderr}"
+
+        # We want to see live logs from bash scripts so inside
+        # bash scripts we have 'set -xe' declaration. However, this
+        # prints the logs to stderr. So here we merge stdout and stderr
+        # outputs into the same PIPE and log them line by line.
+        p = subprocess.Popen(['bash', bash_test], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        output = []
+        for line in iter(p.stdout.readline, b''):
+            print(line)
+            output.append(str(line))
+        p.stdout.close()
+        p.wait()
+
+        error_msg = ""
+        if p.returncode != 0:
+            error_msg = "\n".join(output[-50:])
+
+        assert p.returncode == 0, f"{test_file} failed (exit code is different than 0). Last 50 lines from output: {error_msg}"
 
     return _run_bash_test

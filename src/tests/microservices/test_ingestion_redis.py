578547ed621b7d42e9e666a5808cc382d5133868
Mateusz Blaszkowski <mateusz.blaszkowski@intel.com>
2024-09-18 21:45:59 +0200

    [Validation] Test ingestion microservice (#130)
    * Test ingestion-redis
    
    Signed-off-by: Mateusz Blaszkowski <mateusz.blaszkowski@intel.com>
    
    * hotfix return value for vectorstore
    
    * Verify if Redis has been populated properly
    
    Signed-off-by: Mateusz Blaszkowski <mateusz.blaszkowski@intel.com>
    
    * Restore build and cleanup phases
    
    Signed-off-by: Mateusz Blaszkowski <mateusz.blaszkowski@intel.com>
    
    ---------
    
    Signed-off-by: Mateusz Blaszkowski <mateusz.blaszkowski@intel.com>
    Co-authored-by: Michał Prostko <michal.prostko@intel.com>

---
+++ src/tests/microservices/test_ingestion_redis.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+import allure
+
+ORIGINAL_TEST = "test_ingestion_redis.sh"
+
+
+@allure.link("https://jira.devtools.intel.com/secure/Tests.jspa#/testCase/IEASG-T10")
+def test_ingestion_redis(assert_bash_test_succeeds):
+    assert_bash_test_succeeds(ORIGINAL_TEST)

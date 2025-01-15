58169e1db6bb02207fd0bcb8002cd408e93a9936
intelpljanot <patryk.l.janota@intel.com>
2025-01-15 08:24:41 +0100

    * Tox passing KUBECONFIG var if validating non default ~/.kube/config (#631)
    * edp-minio-provisioning pod is always in Succeeded phase (never
      Running - exception)

--- src/tests/e2e/test_pods_running.py
+++ src/tests/e2e/test_pods_running.py
@@ -8,12 +8,11 @@ import allure
 import kr8s
 import pytest
 
-
 @pytest.mark.smoke
 @allure.testcase("IEASG-T27")
 def test_pods_are_running():
     pods_not_running = []
-    for pod in kr8s.get("pods", namespace=kr8s.ALL):
-        if not pod.status.phase == "Running":
-            pods_not_running.append(f"{pod.namespace}/{pod.name}")
-    assert pods_not_running == [], "Some of the pods are not in running state"
+    for pod in kr8s.get("pods", namespace=kr8s.ALL): # type: kr8s.api_resources.Pod
+        if not pod.status.phase == "Running" and not pod.status.phase == "Succeeded":
+                pods_not_running.append(f"{pod.namespace}/{pod.name} is in {pod.status.phase}")
+    assert pods_not_running == [], "Some of the pods are not in Running or Succeeded state"
